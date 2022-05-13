d60a86cb2a3266caa985b4ee762055313c87147c
Sergieo <sergieol23@gmail.com>
2022-05-13 02:35:34 -0500

    comment model created

--- models/index.js
+++ models/index.js
@@ -1,6 +1,7 @@
 // imports Models and exports as objects with it as a property
 const User = require('./User');
 const Post = require('./Post');
+const Comment = require('./Comment');
 
 // create associations
 
@@ -14,4 +15,24 @@ Post.belongsTo(User, {
   foreignKey: 'user_id',
 });
 
-module.exports = { User, Post };
+// a comment can only have one user
+Comment.belongsTo(User, {
+    foreignKey: 'user_id'
+})
+
+// a comment can have only one post_id
+Comment.belongsTo(Post, {
+    foreignKey: 'post_id'
+})
+
+// a user can have many comments
+User.hasMany(Comment, {
+    foreignKey: 'user_id'
+})
+
+// a post can have many comments
+Post.hasMany(Comment, {
+    foreignKey: 'post_id'
+})
+
+module.exports = { User, Post, Comment };
