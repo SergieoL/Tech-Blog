23ff6d30dc409a17e12cb6da314404250b340678
Sergieo <sergieol23@gmail.com>
2022-05-13 18:52:13 -0500

    Rednderd homepage

--- controllers/index.js
+++ controllers/index.js
@@ -1,9 +1,13 @@
 // collects the packaged API routes
 const router = require('express').Router();
 
+const homeRoutes = require('./home-routes');
+
 const apiRoutes = require('./api');
 
 router.use('/api', apiRoutes);
+
+router.use('/', homeRoutes);
 
 // recieve a 404 error if a request is mafe to any endpoint that doesn't exist
 router.use((req, res) => {
--- server.js
+++ server.js
@@ -1,12 +1,26 @@
 const express = require('express');
-const routes = require('./routes');
+const routes = require('./controllers');
 const sequelize = require('./config/connection');
+// makes stylesheet avaialable to server
+const path = require('path');
 
 const app = express();
 const PORT = process.env.PORT || 3001;
 
+// handlebars start
+const exphbs = require('express-handlebars');
+const hbs = exphbs.create({});
+
+app.engine('handlebars', hbs.engine);
+app.set('view engine', 'handlebars');
+// handlebars end
+
 app.use(express.json());
 app.use(express.urlencoded({ extended: true }));
+
+// middleware function to take contents of 'public' folder and serve them as static assests
+app.use(express.static(path.join(__dirname, 'public')));
+
 
 // turn on routes
 app.use(routes);
