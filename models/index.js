99123ff919afbefe937017d144c830c6b8b1c1a6
Sergieo <sergieol23@gmail.com>
2022-05-13 02:00:40 -0500

    Added Post model

--- models/index.js
+++ models/index.js
@@ -2,4 +2,16 @@
 const User = require('./User');
 const Post = require('./Post');
 
+// create associations
+
+// a user can have many posts
+User.hasMany(Post, {
+    foreignKey: 'user_id'
+  });
+
+// a post can only have one user
+Post.belongsTo(User, {
+  foreignKey: 'user_id',
+});
+
 module.exports = { User, Post };
