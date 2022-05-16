63a2726e322ec07436cb8a80d919c46259808ac8
Sergieo <sergieol23@gmail.com>
2022-05-15 22:50:11 -0500

    fixed jawsdb connection authentication

--- config/connection.js
+++ config/connection.js
@@ -1,3 +1,7 @@
+const Sequelize = require('sequelize');
+
+require('dotenv').config();
+
 let sequelize;
 
 if (process.env.JAWSDB_URL) {
@@ -10,19 +14,4 @@ if (process.env.JAWSDB_URL) {
   });
 }
 
-// // import the Sequelize constructor from the library
-// const Sequelize = require('sequelize');
-
-// require('dotenv').config();
-
-// // create connection to our database
-// const sequelize = new Sequelize(process.env.DB_NAME, process.env.DB_USER, process.env.DB_PASSWORD, {
-//     host: 'localhost',
-//     dialect: 'mysql',
-//     port: 3306
-// });
-
-// module.exports = sequelize;
-
-
-
+module.exports = sequelize;
